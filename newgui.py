ed91c1a5b684d924d7d2bc4f8018873eafbdf7d2
Bainard <sirso.fell@protonmail.com>
2020-02-18 13:46:47 +0100

    catch variable user_choice

--- newgui.py
+++ newgui.py
@@ -19,11 +19,11 @@ class SeaofBTCapp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         self.value1 = tk.StringVar()
-
+        self.user_choice = ""
         self.frames = {}
-        all_frames = (StartPage, ChooseCate, ChooseFood,)# Results, Substitute, SaveSubs)
+        self.all_frames = (StartPage, ChooseCate, ChooseFood)# Results, Substitute, SaveSubs)
 
-        for F in all_frames:
+        for F in self.all_frames:
 
             frame = F(container, self)
 
@@ -37,8 +37,8 @@ class SeaofBTCapp(tk.Tk):
 
         frame = self.frames[cont]
         frame.tkraise()
-    def get_page(self, page_class):
-        return self.frames[page_class]
+    # def get_page(self, page_class):
+    #     return self.frames[page_class]
 
 class StartPage(tk.Frame):
 
@@ -60,30 +60,33 @@ class ChooseCate(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        # self.controller = controller
         label = tk.Label(self, text="Choix de l'aliment a substituer", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         vals = ["jambon", "chocolat", "biscuit"]
         etiqs = ["jambon", "chocolat", "biscuit"]
+
         self.varCatch = tk.StringVar()    #define type of variable catching
-        self.varCatch.set("toto")     #set variable to chocolat?
-        self.entry1 = self.controller.shared_data["choice"]
-        self.entry1.set("toto")
+
         self.choice = None
+        def get_value():
+            controller.user_choice = self.varCatch.get()
+            frame = ChooseFood(parent,controller)
+            controller.frames[ChooseFood] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+
         for i in range(3):
-            b = tk.Radiobutton(self, variable = self.varCatch, text= etiqs[i], value = vals[i], command= self.sel)
+            b = tk.Radiobutton(self, variable = self.varCatch, text= etiqs[i], value = vals[i], command= lambda : [get_value(), controller.show_frame(ChooseFood)] )
             b.pack(side="top", expand=1)
 
         button1 = tk.Button(self, text="Retour au menu",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
-        button2 = tk.Button(self, text="ok",
-                            command=lambda: self.controller.show_frame(ChooseFood))
-        button2.pack()
-    def sel(self):
-        self.varCatch.get()
-        print(self.varCatch.get())
-        self.controller.shared_data["choice"]=self.controller.shared_data["choice"].get()
-        print(self.controller.shared_data["choice"])
+
+
+
+
 
         # self.controller.show_frame(ChooseFood)
 
@@ -92,10 +95,11 @@ class ChooseFood(tk.Frame):
 
     def __init__(self, parent, controller):
         self.controller =controller
-        page1 = self.controller.get_page(ChooseCate)
-        self.toto = self.controller.shared_data["choice"]
+        # page1 = self.controller.get_page(ChooseCate)
+        self.toto = self.controller.user_choice
+        print(self.toto)
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text=str(page1.varCatch.get()), font=LARGE_FONT)
+        label = tk.Label(self, text=str(self.toto), font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
         button1 = tk.Button(self, text="Back to Home",
